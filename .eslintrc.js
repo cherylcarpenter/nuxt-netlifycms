7984dc3a387d6a11df1308c3aa6f632c46b61b9d
Cheryl Carpenter <cherylcarpenter2015@gmail.com>
2021-10-01 14:09:44 -0500

    working on storybook tailwind

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,15 @@
 module.exports = {
   root: true,
   env: {
-    browser: true,
     node: true
   },
-  extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended',
-    'prettier'
-  ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
+  extends: ['@nuxtjs', '@nuxtjs/eslint-config-typescript', 'prettier'],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
 }
