c4d3005bd1a07cfedff07022eac43b705f049a17
Cheryl Carpenter <cherylcarpenter2015@gmail.com>
2021-10-04 16:54:25 -0500

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,7 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ['@nuxtjs', '@nuxtjs/eslint-config-typescript', 'prettier'],
-  parserOptions: {
-    parser: '@typescript-eslint/parser',
-    ecmaVersion: 2020
-  },
+  extends: ['@nuxtjs', 'prettier'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
